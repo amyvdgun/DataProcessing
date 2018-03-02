ec22f4b1fe958774f1a2de1352cf52cfae89b33f
Amy van der Gun <amyvdgun@live.nl>
2018-03-03 00:19:10 +0100

    change

--- Homework/Week5/d3line.js
+++ Homework/Week5/d3line.js
@@ -6,3 +6,28 @@
 *
 *
 */
+
+// load in data form JSON files
+queue()
+	.defer(d3.json, "data.json")
+	.defer(d3.json, "data2017.json")
+	.await(function(data2016, data2017) {
+		
+		// convert data into numbers
+		data2016.forEach(function(d) {
+    		d.Immigratie = parseInt(d.Immigratie);
+    		d.Emigratie = parseInt(d.Emigratie);
+    		d.TotaleBevolkingsgroei = parseInt(d.TotaleBevolkingsgroei);
+  		});
+
+		// convert data into numbers
+  		data2017.forEach(function(d) {
+  			d.Immigratie = parseInt(d.Immigratie);
+  			d.Emigratie = parseInt(d.Emigratie);
+  			d.TotaleBevolkingsgroei = parseInt(d.TotaleBevolkingsgroei);
+  		});
+
+  		console.log(data2016);
+  		console.log(data2017);
+ 	});
+
