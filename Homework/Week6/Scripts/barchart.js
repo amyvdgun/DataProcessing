5b213107a5d0fb467c8273f18618c77e1c0eef3c
Amy van der Gun <amyvdgun@live.nl>
2018-03-23 23:56:12 +0100

    updatee

--- Homework/Week6/Scripts/barchart.js
+++ Homework/Week6/Scripts/barchart.js
@@ -1,200 +1,210 @@
-/** 
-* Amy van der Gun
-* 10791760
-* 
-* barchart.js
-*
-* Creates an interactive barchart using data from a json file.
-*
-* With help from: 
-* https://bl.ocks.org/syncopika/f1c9036b0deb058454f825238a95b6be 
-*/
+/**
+ * Amy van der Gun
+ * 10791760
+ *
+ * barchart.js
+ *
+ * Creates an interactive barchart using data from a json file.
+ *
+ * With help from:
+ * https://bl.ocks.org/syncopika/f1c9036b0deb058454f825238a95b6be
+ */
 
 // creates the barchart using a json file and users input
 function createBarchart(alldata, chosenState) {
-  
-  // set the outer and inner width and height
-  var margin = {top: 40, bottom: 40, left: 200, right: 200},
-    height = 500 - margin.top - margin.bottom,
-    width = 1100 - margin.left - margin.right;
 
-  // set the chart sizes
-  var chart = d3.select("#barchart")
-    .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    // set the outer and inner width and height
+    var margin = {top: 40, bottom: 40, left: 200, right: 200},
+        height = 500 - margin.top - margin.bottom,
+        width = 1100 - margin.left - margin.right;
 
-  // create interactive title
-  document.getElementById("barchartTitle").innerHTML = "Babies born in " + chosenState;
+    // set the chart sizes
+    var chart = d3.select("#barchart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // add the tooltip and its content
-  var tip = d3.tip()
-    .attr("class", "d3-tip")
-    .offset([-10, 0])
-    .html(function (d) {
-      var formatting = d3.format(",");
-      return (formatting(d))});
+    // create interactive title
+    document.getElementById("barchartTitle").innerHTML = "Babies born in " + chosenState;
 
-  // start the tip
-  chart.call(tip);
+    // add the tooltip and its content
+    var tip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([-10, 0])
+        .html(function (d) {
+            var formatting = d3.format(",");
+            return (formatting(d))
+        });
 
-  // create arrays for the json data
-  var years = ["2011", "2012", "2013", "2014", "2015", "2016", "2017"];
-  var babies = [];
+    // start the tip
+    chart.call(tip);
 
-  // iterate over the data and push to the right array
-  for (var i = 0; i < alldata.length; i++) {
-    if (alldata[i].StateName === chosenState) {
-      babies.push(alldata[i].Births2011);
-      babies.push(alldata[i].Births2012);
-      babies.push(alldata[i].Births2013);
-      babies.push(alldata[i].Births2014);
-      babies.push(alldata[i].Births2015);
-      babies.push(alldata[i].Births2016);
-      babies.push(alldata[i].Births2017);
+    // create arrays for the json data
+    var years = ["2011", "2012", "2013", "2014", "2015", "2016", "2017"];
+    var babies = [];
+
+    // iterate over the data and push to the right array
+    for (var i = 0; i < alldata.length; i++) {
+        if (alldata[i].StateName === chosenState) {
+            babies.push(alldata[i].Births2011);
+            babies.push(alldata[i].Births2012);
+            babies.push(alldata[i].Births2013);
+            babies.push(alldata[i].Births2014);
+            babies.push(alldata[i].Births2015);
+            babies.push(alldata[i].Births2016);
+            babies.push(alldata[i].Births2017);
+        }
     }
-  }
 
-  // set the range and domain for x 
-  var x = d3.scaleBand()
-    .range([0, width], .1)
-    .padding(0.4)
-    .domain(years);
+    // set the range and domain for x
+    var x = d3.scaleBand()
+        .range([0, width], .1)
+        .padding(0.4)
+        .domain(years);
 
-  // set the range and domain for y
-  var y = d3.scaleLinear()
-    .domain([0, d3.max(babies)])
-    .range([height, 0]);
+    // set the range and domain for y
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(babies)])
+        .range([height, 0]);
 
-  // create and draw x-axis on desired position
-  var xAxis = d3.axisBottom(x);
-  chart.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
+    // create and draw x-axis on desired position
+    var xAxis = d3.axisBottom(x);
+    chart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-  // create and draw y-axis on desired position
-  var yAxis = d3.axisLeft(y);
-  chart.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
+    // create and draw y-axis on desired position
+    var yAxis = d3.axisLeft(y);
+    chart.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
 
-  // set label of y axis
-  chart.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("x", 0)
-    .attr("y", -margin.left / 2.5)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .style("font", "12px sans-serif")
-    .text("Amount of births");
+    // set label of y axis
+    chart.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", 0)
+        .attr("y", -margin.left / 2.5)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .style("font", "12px sans-serif")
+        .text("Amount of births");
 
-  // add bars with linked data to the chart
-  chart.selectAll(".bar")
-    .data(babies)
-  .enter().append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d, i) { return x(years[i]); })
-    .attr("y", function(d) { return y(d); })
-    .attr("height", function(d) { return height - y(d); })
-    .attr("width", x.bandwidth())
-    .on("mouseover", tip.show)
-    .on("mouseout", tip.hide)
-    .style("fill", "#AF9483");
+    // add bars with linked data to the chart
+    chart.selectAll(".bar")
+        .data(babies)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d, i) {
+            return x(years[i]);
+        })
+        .attr("y", function (d) {
+            return y(d);
+        })
+        .attr("height", function (d) {
+            return height - y(d);
+        })
+        .attr("width", x.bandwidth())
+        .on("mouseover", tip.show)
+        .on("mouseout", tip.hide)
+        .style("fill", "#AF9483");
 }
 
 // this functions updates the barchart when user clicks on particular state
 function update(alldata, state) {
 
-  // set the outer and inner width and height
-  var margin = {top: 40, bottom: 40, left: 200, right: 200},
-    height = 500 - margin.top - margin.bottom,
-    width = 1100 - margin.left - margin.right;
+    // set the outer and inner width and height
+    var margin = {top: 40, bottom: 40, left: 200, right: 200},
+        height = 500 - margin.top - margin.bottom,
+        width = 1100 - margin.left - margin.right;
 
-  // select the element to change and store in chart variable
-  var chart = d3.select("#barchart").select("svg").select("g");
+    // select the element to change and store in chart variable
+    var chart = d3.select("#barchart").select("svg").select("g");
 
-  // let the title change
-  document.getElementById("barchartTitle").innerHTML = "Babies born in " + state;
+    // let the title change
+    document.getElementById("barchartTitle").innerHTML = "Babies born in " + state;
 
-  // create empty array
-  var babies = [];
+    // create empty array
+    var babies = [];
 
-  // iterate over the data and push state data to the babies array
-  for (var i = 0; i < alldata.length; i++) {
-    if (alldata[i].StateName === state) {
-      babies.push(alldata[i].Births2011);
-      babies.push(alldata[i].Births2012);
-      babies.push(alldata[i].Births2013);
-      babies.push(alldata[i].Births2014);
-      babies.push(alldata[i].Births2015);
-      babies.push(alldata[i].Births2016);
-      babies.push(alldata[i].Births2017);
+    // iterate over the data and push state data to the babies array
+    for (var i = 0; i < alldata.length; i++) {
+        if (alldata[i].StateName === state) {
+            babies.push(alldata[i].Births2011);
+            babies.push(alldata[i].Births2012);
+            babies.push(alldata[i].Births2013);
+            babies.push(alldata[i].Births2014);
+            babies.push(alldata[i].Births2015);
+            babies.push(alldata[i].Births2016);
+            babies.push(alldata[i].Births2017);
+        }
     }
-  }
 
-  // add the tooltip and its content
-  var tip = d3.tip()
-    .attr("class", "d3-tip")
-    .offset([-10, 0])
-    .html(function (d) {
-      var formatting = d3.format(",");
-      return (formatting(d))});
+    // add the tooltip and its content
+    var tip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([-10, 0])
+        .html(function (d) {
+            var formatting = d3.format(",");
+            return (formatting(d))
+        });
 
-  // start the tip
-  chart.call(tip);
+    // start the tip
+    chart.call(tip);
 
-  // set the range and domain for y
-  var y = d3.scaleLinear()
-    .domain([0, d3.max(babies)])
-    .range([height, 0]);
+    // set the range and domain for y
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(babies)])
+        .range([height, 0]);
 
-  // create and draw y-axis on desired position and set label
-  var yAxis = d3.axisLeft(y);
+    // create and draw y-axis on desired position and set label
+    var yAxis = d3.axisLeft(y);
 
-  // call y axis and add transition
-  d3.select(".y.axis")
-    .transition()
-    .duration(1000)
-    .call(yAxis)
+    // call y axis and add transition
+    d3.select(".y.axis")
+        .transition()
+        .duration(1000)
+        .call(yAxis)
 
-  // select bars and link data 
-  var bars = chart.selectAll(".bar")
-    .data(babies)
-    .on("mouseover", tip.show)
-    .on("mouseout", tip.hide)
-    .style("fill", "#AF9483");
+    // select bars and link data
+    var bars = chart.selectAll(".bar")
+        .data(babies)
+        .on("mouseover", tip.show)
+        .on("mouseout", tip.hide)
+        .style("fill", "#AF9483");
 
-  // link new y coordinates to bars and add interactivity and transition
-  bars
-    .transition().duration(1000)
-    .attr("y", function(d) { return y(d); })
-    .attr("height", function(d) { return height - y(d); })
-    .style("fill", "#AF9483");
+    // link new y coordinates to bars and add interactivity and transition
+    bars
+        .transition().duration(1000)
+        .attr("y", function (d) {
+            return y(d);
+        })
+        .attr("height", function (d) {
+            return height - y(d);
+        })
+        .style("fill", "#AF9483");
 }
 
-function searchState(alldata) {
+function searchState() {
+    d3.queue()
+        .defer(d3.json, "us.json")
+        .defer(d3.json, "alldata.json")
+        .await(function (error, us, alldata) {
+            var input = document.getElementById("inputState");
+            var filter = input.value.toUpperCase();
+            var found = false;
 
-    var input = document.getElementById("inputState");
-    var filter = input.value.toUpperCase();
+            for (var i = 0; i < us.features.length; i++) {
+                if (us.features[i].properties.name.toUpperCase() === filter) {
+                    update(alldata, us.features[i].properties.name);
+                    found = true;
+                }
+            }
 
-    console.log(input);
-    console.log(filter);
-
-    // // Loop through all list items, and hide those who don't match the search query
-    // for (i = 0; i < li.length; i++) {
-    //     a = li[i].getElementsByTagName("a")[0];
-    //     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-    //         li[i].style.display = "";
-    //     } else {
-    //         li[i].style.display = "none";
-    //     }
-    // }
+            if (!found) {
+                alert('Niet gevonden!!!');
+            }
+        });
 }
-
-
-
-
-
--- Homework/Week6/Scripts/map.js
+++ Homework/Week6/Scripts/map.js
@@ -1,164 +1,162 @@
-/** 
-* Amy van der Gun
-* 10791760
-* 
-* map.js
-*
-* Creates an interactive map of the USA.
-*
-* With help from:
-* https://bl.ocks.org/wboykinm/dbbe50d1023f90d4e241712395c27fb3
-*/
+/**
+ * Amy van der Gun
+ * 10791760
+ *
+ * map.js
+ *
+ * Creates an interactive map of the USA.
+ *
+ * With help from:
+ * https://bl.ocks.org/wboykinm/dbbe50d1023f90d4e241712395c27fb3
+ */
 
 // execute function when DOM is loaded
-window.onload = function() {
+window.onload = function () {
 
-  // set the outer and inner width and height
-  var margin = {top: 20, bottom: 20, left: 20, right: 20},
-      height = 500 - margin.top - margin.bottom,
-      width = 1100 - margin.left - margin.right;
+    // set the outer and inner width and height
+    var margin = {top: 20, bottom: 20, left: 20, right: 20},
+        height = 500 - margin.top - margin.bottom,
+        width = 1100 - margin.left - margin.right;
 
-  // set the colors corresponding to the highest and lowest values
-  var lowColor = "#f9f9f9"
-  var highColor = "#92B558"
+    // set the colors corresponding to the highest and lowest values
+    var lowColor = "#f9f9f9"
+    var highColor = "#92B558"
 
-  // scale and center to the screen
-  var projection = d3.geoAlbersUsa()
-    .translate([width / 2, height / 2])
-    .scale([1000]);
+    // scale and center to the screen
+    var projection = d3.geoAlbersUsa()
+        .translate([width / 2, height / 2])
+        .scale([1000]);
 
-  // create path
-  var path = d3.geoPath()
-    .projection(projection);
+    // create path
+    var path = d3.geoPath()
+        .projection(projection);
 
-  // create the tooltip for interactivity and set its content
-  var tip = d3.tip()
-      .attr("class", "d3-tip")
-      .offset([-10, 0])
-      .html(function (d) {
-        var formatting = d3.format(",");
-        return ("State: " + d.properties.name + "<br>" + "Population: " + formatting(d.properties.value))});
+    // create the tooltip for interactivity and set its content
+    var tip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([-10, 0])
+        .html(function (d) {
+            var formatting = d3.format(",");
+            return ("State: " + d.properties.name + "<br>" + "Population: " + formatting(d.properties.value))
+        });
 
-  // add the SVG element and set characteristics
-  var svg = d3.select("#map")
-    .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    // add the SVG element and set characteristics
+    var svg = d3.select("#map")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // call the tooltip
-  svg.call(tip);
+    // call the tooltip
+    svg.call(tip);
 
-  // load in two json files 
-  d3.queue()
-    .defer(d3.json, "us.json")
-    .defer(d3.json, "alldata.json")
-    .await(function (error, us, alldata) {
+    // load in two json files
+    d3.queue()
+        .defer(d3.json, "us.json")
+        .defer(d3.json, "alldata.json")
+        .await(function (error, us, alldata) {
 
-      // throw error if files cannot be loaded
-      if (error) throw error;
+            // throw error if files cannot be loaded
+            if (error) throw error;
 
-      // create an empty array and push the population data in it
-      var dataArray = [];
-      alldata.forEach(function (d) {
-        d.Population = parseInt(d.Population);
-        dataArray.push(+d.Population);
-      });
+            // create an empty array and push the population data in it
+            var dataArray = [];
+            alldata.forEach(function (d) {
+                d.Population = parseInt(d.Population);
+                dataArray.push(+d.Population);
+            });
 
-      // create barchart and use Alabama as the default state
-      createBarchart(alldata, "Alabama");
+            // create barchart and use Alabama as the default state
+            createBarchart(alldata, "Alabama");
 
-      // determine the extreme values and set domain and range
-      var min = d3.min(dataArray)
-      var max = d3.max(dataArray)
-      var color = d3.scaleLinear().domain([min,max]).range([lowColor,highColor])
-      
-      // iterate over the data file and separate into name and value
-      for (var i = 0; i < alldata.length; i++) {
-        var dataState = alldata[i].StateName;
-        var dataValue = alldata[i].Population;
+            // determine the extreme values and set domain and range
+            var min = d3.min(dataArray)
+            var max = d3.max(dataArray)
+            var color = d3.scaleLinear().domain([min, max]).range([lowColor, highColor])
 
-        // iteratue over the us data file and store state name in variable
-        for (var j = 0; j < us.features.length; j++) {
-          var jsonState = us.features[j].properties.name;
+            // iterate over the data file and separate into name and value
+            for (var i = 0; i < alldata.length; i++) {
+                var dataState = alldata[i].StateName;
+                var dataValue = alldata[i].Population;
 
-          // link the population value if state names in the two files match
-          if (dataState == jsonState) {
-            us.features[j].properties.value = dataValue;
-            break;
-        }
-      }
-    }
+                // iteratue over the us data file and store state name in variable
+                for (var j = 0; j < us.features.length; j++) {
+                    var jsonState = us.features[j].properties.name;
 
-    // put the data into the SVG and create path
-    svg.selectAll("path")
-      .data(us.features)
-      .enter()
-      .append("path")
-      .attr("d", path)
-      .style("stroke", "#fff")
-      .style("stroke-width", "1")
-      .style("fill", function(d) { return color(d.properties.value) })
-      .on("mouseover", tip.show)
-      .on("mouseout", tip.hide)
-      .on("click", function(d) { var chosenState = d.properties.name;
-        update(alldata, chosenState); });
+                    // link the population value if state names in the two files match
+                    if (dataState == jsonState) {
+                        us.features[j].properties.value = dataValue;
+                        break;
+                    }
+                }
+            }
 
-    // create the legend
-    var key = d3.select("svg")
-      .append("svg")
-      .attr("width", 140)
-      .attr("height", 150)
-      .attr("class", "legend");
+            // put the data into the SVG and create path
+            svg.selectAll("path")
+                .data(us.features)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .style("stroke", "#fff")
+                .style("stroke-width", "1")
+                .style("fill", function (d) {
+                    return color(d.properties.value)
+                })
+                .on("mouseover", tip.show)
+                .on("mouseout", tip.hide)
+                .on("click", function (d) {
+                    var chosenState = d.properties.name;
+                    update(alldata, chosenState);
+                });
 
-    var legend = key.append("defs")
-      .append("svg:linearGradient")
-      .attr("id", "gradient")
-      .attr("x1", "100%")
-      .attr("y1", "0%")
-      .attr("x2", "100%")
-      .attr("y2", "100%")
-      .attr("spreadMethod", "pad");
+            // create the legend
+            var key = d3.select("svg")
+                .append("svg")
+                .attr("width", 140)
+                .attr("height", 150)
+                .attr("class", "legend");
 
-    // set top of legend
-    legend.append("stop")
-      .attr("offset", "0%")
-      .attr("stop-color", highColor)
-      .attr("stop-opacity", 1);
-      
-    // set bottom of legend
-    legend.append("stop")
-      .attr("offset", "100%")
-      .attr("stop-color", lowColor)
-      .attr("stop-opacity", 1);
+            var legend = key.append("defs")
+                .append("svg:linearGradient")
+                .attr("id", "gradient")
+                .attr("x1", "100%")
+                .attr("y1", "0%")
+                .attr("x2", "100%")
+                .attr("y2", "100%")
+                .attr("spreadMethod", "pad");
 
-    // append rectangle for the legend
-    key.append("rect")
-      .attr("width", 20)
-      .attr("height", 150)
-      .style("fill", "url(#gradient)")
-      .attr("transform", "translate(20,10)");
+            // set top of legend
+            legend.append("stop")
+                .attr("offset", "0%")
+                .attr("stop-color", highColor)
+                .attr("stop-opacity", 1);
 
-    // create y variable
-    var y = d3.scaleLinear()
-      .range([150, 0])
-      .domain([min, max]);
+            // set bottom of legend
+            legend.append("stop")
+                .attr("offset", "100%")
+                .attr("stop-color", lowColor)
+                .attr("stop-opacity", 1);
 
-    // create y axis
-    var yAxis = d3.axisRight(y);
+            // append rectangle for the legend
+            key.append("rect")
+                .attr("width", 20)
+                .attr("height", 150)
+                .style("fill", "url(#gradient)")
+                .attr("transform", "translate(20,10)");
 
-    // set y axis
-    key.append("g")
-      .attr("class", "yaxis")
-      .attr("transform", "translate(41,10)")
-      .call(yAxis)
-    });
+            // create y variable
+            var y = d3.scaleLinear()
+                .range([150, 0])
+                .domain([min, max]);
+
+            // create y axis
+            var yAxis = d3.axisRight(y);
+
+            // set y axis
+            key.append("g")
+                .attr("class", "yaxis")
+                .attr("transform", "translate(41,10)")
+                .call(yAxis)
+        });
 };
-
-
-
-
-
-
-
